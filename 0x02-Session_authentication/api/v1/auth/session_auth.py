4f6245386e31e39c6edccff28c5d4cb22442bf12
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2024-09-20 05:40:46 +0300

    Create session_auth.py

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+SessionAuth module
+"""
+import uuid
+from api.v1.auth.auth import Auth
+from models.user import User
+from typing import TypeVar
+
+
+class SessionAuth(Auth):
+    """ SessionAuth class
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a Session ID for a user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ Return a User ID based on a Session ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None) -> User:
+        """ Current User method based on Session ID
+        """
+        if request is None:
+            return None
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+
+        return User.get(user_id)
+
+    def destroy_session(self, request=None) -> bool:
+        """ Destroy Session method
+        """
+        if request is None:
+            return False
+
+        session_id = self.session_cookie(request)
+
+        if session_id is None:
+            return False
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        if user_id is None:
+            return False
+
+        self.user_id_by_session_id.pop(session_id, None)
+        return True
